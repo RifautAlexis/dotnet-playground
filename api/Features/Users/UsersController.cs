c154e9f79a4d48bf01b8e436698d8b063e4341cc
Rifaut Alexis <ralexis149@gmail.com>
2024-04-19 16:59:27 +0200

    WIP organising project with mediatr

---
+++ api/Features/Users/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace api.Features.Users
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet(Name = "GetGamesForConsole")]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            var query = new GetUsersQuery
+            {
+            };
+
+            var result = await _mediator.Send(query);
+
+            return Ok(result);
+        }
+    }
+}
